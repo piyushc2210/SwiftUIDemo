b41d73f105709241f12cc589bdd4f563498a3d43
Piyush Chodvadiya <piyushchodvadiya@Piyushs-MacBook-Pro.local>
2022-05-07 14:30:07 +0530

    first commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Piyush Chodvadiya on 07/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
